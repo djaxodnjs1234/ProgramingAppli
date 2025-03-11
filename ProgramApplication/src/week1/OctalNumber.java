ceef44b9bf13eef20fa69ead72dadcf544998c87
desktop <djaxodnjs1234@naver.com>
2025-03-11 10:57:22 +0900

    8진수는 0~7까지이다.

--- ProgramApplication/src/week1/OctalNumber.java
+++ ProgramApplication/src/week1/OctalNumber.java
@@ -12,7 +12,7 @@ public class OctalNumber {
 
         for(int i=0; i<strLen; i++){
             int intNumber = str.charAt(i) - '0'; // 아스키코드를 정수로 변환하여 쉽게 값 비교
-            if(intNumber>=0 && intNumber<=8){ // 각 자리숫자가 8진수라면?
+            if(intNumber>=0 && intNumber<=7){ // 각 자리숫자가 8진수라면?
                 for(int j=0; j<strLen-i-1; j++){
                     intNumber *= 8; // 각 자리수 곱셈
                 }
